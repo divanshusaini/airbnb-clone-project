f823f131273751215e1416b08a59145c8bf3e985
divashusaini <dishusaini2july@gmail.com>
2025-07-03 10:02:13 +0530

    update code for image updation

--- routes/listing.js
+++ routes/listing.js
@@ -3,17 +3,21 @@ const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
 const Listing = require("../models/listing.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
-
 const listingController = require("../controllers/listings.js");
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
 router
   .route("/")
   .get(wrapAsync(listingController.index))
-  .post(
-    validateListing,
-    isLoggedIn,
-    wrapAsync(listingController.createListing)
-  );
+  // .post(
+  //   validateListing,
+  //   isLoggedIn,
+  //   wrapAsync(listingController.createListing)
+  // );
+  .post(upload.single('listing[image][url]'),(req,res)=>{
+    res.send(req.file);
+  })
 
 //new LIst route
 router.get("/new", isLoggedIn, listingController.renderNewForm);
